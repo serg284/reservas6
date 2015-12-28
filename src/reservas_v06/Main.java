c6d93eda604e81a1bebd60d5a2007cbe2ef12001
Sergio <serg.ribeiro@gmail.com>
2015-12-28 19:59:07 +0000

    commit01

---
+++ src/reservas_v06/Main.java
@@ -0,0 +1,34 @@
+package video80;
+
+//video80 , default
+public class Main {
+
+    public static void main(String[] args) {
+    System.out.println("hello1");
+
+        //lerSalas
+        System.out.println("*** comeca lerSalas ***");
+        lerSalas s = new lerSalas();
+        s.openFile();
+        s.readFile();
+        s.closeFile();
+
+        //lerReservas
+        System.out.println("*** comeca lerReservas ***");
+        lerReservas r = new lerReservas();
+        r.openFile();
+        r.readFile();
+        r.closeFile();
+
+        //criarOutput
+        System.out.println("*** comeca criarOutput ***");
+        criarOutput g = new criarOutput();
+        g.openFile();
+        g.addRecords();
+        g.closeFile();
+
+
+
+
+    }
+}
